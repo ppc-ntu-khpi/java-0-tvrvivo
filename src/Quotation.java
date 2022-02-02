b2ddd1af5ec58c8dfaf262a92589920a76070de1
Василий Водолага <91936834+tvrvivo@users.noreply.github.com>
2022-02-02 19:07:50 +0000

    Additional tasks completed

--- src/Quotation.java
+++ src/Quotation.java
@@ -1,6 +1,8 @@
 public class Quotation {
   String quote = "\nStrive not to be a success, but rather to be of value!";
+  String author = "\t\t\t\t\t\t\t\t\t\t\t -Albert Einstein";
   public void display() {
     System.out.println(quote);
+    System.out.println(author);
   }
 }
