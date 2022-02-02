e9af80feff07133d343d06408819426245a00384
Василий Водолага <91936834+tvrvivo@users.noreply.github.com>
2022-02-02 19:01:58 +0000

    Task 2 completed

--- src/Quotation.java
+++ src/Quotation.java
@@ -1,5 +1,5 @@
 public class Quotation {
-  String quote = "\nWelcome to Sun!";
+  String quote = "\nStrive not to be a success, but rather to be of value!";
   public void display() {
     System.out.println(quote);
   }
